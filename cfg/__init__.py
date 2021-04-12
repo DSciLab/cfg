67d3e3d9797a7c9a5b50d819ad98da9ff19a810c
tor4z <vwenjie@hotmail.com>
2021-04-13 00:05:35 +0800

    Update setup info

--- cfg/__init__.py
+++ cfg/__init__.py
@@ -4,6 +4,19 @@ import inspect
 import yaml
 from .yaml_loader import YAMLLoader
 from .argparser import Args
+import random
+
+
+def gen_code():
+    d = datetime.datetime.now()
+    date_str = d.strftime('%Y%m%d_%H%M%S')
+    code = ''.join(random.choices('0123456789qwertyui' + \
+                                'opasdfghjklzxcvbnmQWERT' + \
+                                'YUIOPASDFGHJKLZXCVBNM', k=5))
+    return f'{code}-{date_str}'
+
+
+__version__ = f'0.1-{gen_code()}'
 
 
 class Opts(Args):
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
+import cfg
 
 
-setup(name='cfg',
-      version='0.0',
+setup(name='cfg_dist',
+      version=cfg.__version__,
       description='Auto configuration',
       author='tor4z',
       author_email='vwenjie@hotmail.com',
+      install_requires=[
+            'PyYAML'
+      ],
       packages=['cfg'],
      )
